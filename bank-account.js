e325045e45fd5968a660646c15be336561dc732a
Orielsy Diaz <orielsydiaz@gmail.com>
2017-02-06 23:39:20 +0000

    end of day

--- bank-account.js
+++ bank-account.js
@@ -1,50 +1,87 @@
 var accounts = [];
 
 var createAccount = function(username, balance){
-    var account = {
-        username: username,
-        balance: balance
+    var account = {};
+    if(typeof balance === 'number'){
+        account = {
+            username: username,
+            balance: balance
+        };
+        accounts.push(account);
+    } else{
+        console.log("Balance given is not a number");
     }
-    accounts.push(account);
+    return account;
 };
 
 var getAccount = function(username){
-    var find;
-    accounts.forEach(function(current, index){
-        if(current.username === username){
-            find = current;
+    var matchedAccount;
+    
+    for(var x = 0; x < accounts.length; x++){
+        if(accounts[x].username === username){
+            matchedAccount = accounts[x];
         } 
-    });
-    return find;
+    }
+    return matchedAccount;
 };
 
-var deposit = function (acc, amount){
-    acc.balance += amount;
+var deposit = function (username, amount){
+    
+    if(typeof amount === 'number'){
+        for (var x = 0; x < accounts.length; x++){
+            if(accounts[x].username === username){
+                accounts[x].balance += amount;    
+            }
+        }
+    } else {
+        console.log('Ammount is not a number. Please use number.');
+    }
 };
 
 //withdraw
-var withdraw = function (acc, amount){
-    acc.balance -= amount;
+var withdraw = function (username, amount){
+    if(typeof amount === 'number'){
+        for (var x = 0; x < accounts.length; x++){
+            if(accounts[x].username === username){
+                accounts[x].balance -= amount;       
+            }
+        }
+    } else {
+        console.log('Ammount is not a number. Please use number.');
+    }
 };
 
 //getBalance
-var getBalance = function (acc){
-    return acc.balance;
+var getBalance = function (username){
+    for(var x = 0; x < accounts.length; x++){
+        if(accounts[x].username === username){
+            return accounts[x].balance;       
+        }
+    }
 };
 
-
-createAccount('ori', 33);
-createAccount('diaz', 500);
-console.log(getAccount('ori'));
-//deposit
-
-
-var flipper = function(val){
-  if(typeof(val)=== "number"){
-
-  } else {
-
-  }
+var balanceGetter = function(username){
+    return function(){
+        for(var x = 0; x < accounts.length; x++){
+            if(accounts[x].username === username){
+                return accounts[x].balance;       
+            }
+        }
+    };
 };
 
+var displayAll = function(){
+    for(var x = 0; x < accounts.length; x++){
+        console.log('Account #' + x + ": " + accounts[x].username +"---"+ accounts[x].balance);
+    }
+}
 
+createAccount('Orielsy', 'a');
+createAccount('Orielsy', 5);
+createAccount('John', 200);
+withdraw('John', 4);
+deposit('Orielsy', 200);
+console.log(getBalance('Orielsy'));
+console.log(getBalance('John'));
+console.log(getAccount('John'));
+var hold = balanceGetter('Orielsy');
